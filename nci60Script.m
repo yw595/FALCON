8b0f7204ca4a63b57c64ef30fb4c1f1d4216521c
Yiping Wang <yw595@cornell.edu>
2015-06-27 19:08:20 -0400

    added restrictions for small run to nci60Script, and parfor

--- nci60Script.m
+++ nci60Script.m
@@ -1,7 +1,7 @@
-inputDirs = {['NCI60Sims' filesep 'nci60prot'], ...
-    ['NCI60Sims' filesep 'nci60prot'],['NCI60Sims' filesep 'nci60prot_mRNA']};
-outputPrefixes = {'Normal', 'iMAT', 'GIMME', ...
-    'iMATMachado', 'GIMMEMachado'};
+inputDirs = {['NCI60Sims' filesep 'nci60prot']};%, ...
+%    ['NCI60Sims' filesep 'nci60prot'],['NCI60Sims' filesep 'nci60prot_mRNA']};
+outputPrefixes = {'Normal'};%, 'iMAT', 'GIMME', ...
+%    'iMATMachado', 'GIMMEMachado'};
 [cellLinesArray, ~, ~, ~, ~, ] = readJainTable();
 [originTissuesArray INITFilesArray mCADREFilesArray] = makeOriginTissuesArray(cellLinesArray);
 
@@ -14,26 +14,24 @@ for i=1:length(inputDirs)
             system(['mkdir ' outputDir]);
         end
 
-        for k=1:length(cellLinesArray)
+        parfor k=1:2%length(cellLinesArray)
 	    if strcmp(outputPrefixes{j},'Normal')
                 modelToRun = origRecon2;
             else
                 load([inputDir filesep 'specificModels' filesep 'specificModel' cellLinesArray{k} outputPrefixes{j} '.mat']);
 		eval(['modelToRun = specificModel' outputPrefixes{j} ';']);
             end
-            %if ~isempty(regexp(inputFiles(k).name, '.csv$'))
-                runFALCONStripped(constrainMediumExc(initializeRecon2(modelToRun)), ...
-                    [inputDir filesep cellLinesArray{k} '.csv'], 1, outputDir);
-		modelToRun = changeObjective(modelToRun,'biomass_reaction');
-		FBASoln = optimizeCbModel(constrainMediumExc(initializeRecon2(modelToRun)),1);
-		v_fba = FBASoln.x;
-		save([outputDir filesep cellLinesArray{k} '.csv' '_fba_flux.mat'], 'v_fba');
-		fluxOutputFI = fopen([outputDir filesep cellLinesArray{k} '.csv' '.flux'],'w');
-		for l=1:length(v_fba)
-		    fprintf(fluxOutputFI,['R_' modelToRun.rxns{l} ',' num2str(v_fba(l)) '\n']);
-		end
-		fclose(fluxOutputFI);
-            %end
+            runFALCONStripped(constrainMediumExc(initializeRecon2(modelToRun)), ...
+            [inputDir filesep cellLinesArray{k} '.csv'], 1, outputDir);
+	    modelToRun = changeObjective(modelToRun,'biomass_reaction');
+	    FBASoln = optimizeCbModel(constrainMediumExc(initializeRecon2(modelToRun)),1);
+	    v_fba = FBASoln.x;
+	    save([outputDir filesep cellLinesArray{k} '.csv' '_fba_flux.mat'], 'v_fba');
+	    fluxOutputFI = fopen([outputDir filesep cellLinesArray{k} '.csv' '.flux'],'w');
+	    for l=1:length(v_fba)
+		fprintf(fluxOutputFI,['R_' modelToRun.rxns{l} ',' num2str(v_fba(l)) '\n']);
+	    end
+	    fclose(fluxOutputFI);
         end
     end
 end
