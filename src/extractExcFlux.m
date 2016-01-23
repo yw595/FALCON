deccfb808f3411858263f581d53ecd6677e80fcc
yw595 <yipingwang12@gmail.com>
2016-01-23 11:54:38 -0500

    huge reorganization, moved ALL obsolete stuff into safe, .m files into src

---
+++ src/extractExcFlux.m
@@ -0,0 +1,15 @@
+function v_Exc = extractExcFlux(model, v_falcon)
+
+    [~, jainMetsArray, ~] = readJainTable();
+    jainMetsToExcIdxs = loadJainMetsToExcIdxs(jainMetsArray, model);
+    v_Exc = [];
+
+    for j = 1:length(jainMetsArray)
+        jthExcIdxs = jainMetsToExcIdxs(jainMetsArray{j});
+	if ~isempty(jthExcIdxs)
+            v_Exc(j) = sum(v_falcon(jthExcIdxs));
+	else
+	    v_Exc(j) = 0;
+	end
+    end
+end
